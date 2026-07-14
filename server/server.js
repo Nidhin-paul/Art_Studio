49a12d713b053cef46733b06f00cb1781095d5c9
Nidhin <nidhinpaulsonv@gmail.com>
2026-07-14 14:14:55 +0530

    Update CORS to support FRONTEND_URL env var

--- server/server.js
+++ server/server.js
@@ -11,7 +11,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: [
+    'http://localhost:5173', 
+    'http://127.0.0.1:5173', 
+    process.env.FRONTEND_URL
+  ].filter(Boolean),
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   credentials: true,
 }));
